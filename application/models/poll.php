bc70e6c53c470b3cc90fe62e4fbf8f42e0c988da
jolir <oliverjohnpr2011@yahoo.com>
2013-07-25 10:59:38 -0700

    minor cleanup of codes in model

--- application/models/poll.php
+++ application/models/poll.php
@@ -16,42 +16,42 @@ class Poll extends CI_Model {
 	{
 		$this->load->helper('date');
 		$poll_data = array(
-						"title" => $poll_information['poll_title'],
-						"description" => $poll_information['poll_description'],
-						"created_at" => date("Y-m-d H:i:s"),
-						"updated_at" => date("Y-m-d H:i:s")
-					);
+			"title" => $poll_information['poll_title'],
+			"description" => $poll_information['poll_description'],
+			"created_at" => date("Y-m-d H:i:s"),
+			"updated_at" => date("Y-m-d H:i:s")
+		);
 
 		$this->db->insert('polls', $poll_data);
 		
 		$poll_id = $this->db->insert_id();
 
 		$choice_data = array(
-							array(
-								"poll_id" => $poll_id,
-								"choice_text" => $poll_information['poll_choice1'],
-								"created_at" => date("Y-m-d H:i:s"),
-								"updated_at" => date("Y-m-d H:i:s")
-							),
-							array(
-								"poll_id" => $poll_id,
-								"choice_text" => $poll_information['poll_choice2'],
-								"created_at" => date("Y-m-d H:i:s"),
-								"updated_at" => date("Y-m-d H:i:s")							
-							),
-							array(
-								"poll_id" => $poll_id,
-								"choice_text" => $poll_information['poll_choice3'],
-								"created_at" => date("Y-m-d H:i:s"),
-								"updated_at" => date("Y-m-d H:i:s")
-							),
-							array(
-								"poll_id" => $poll_id,
-								"choice_text" => $poll_information['poll_choice1'],
-								"created_at" => date("Y-m-d H:i:s"),
-								"updated_at" => date("Y-m-d H:i:s")
-							)
-					   );
+			array(
+				"poll_id" => $poll_id,
+				"choice_text" => $poll_information['poll_choice1'],
+				"created_at" => date("Y-m-d H:i:s"),
+				"updated_at" => date("Y-m-d H:i:s")
+			),
+			array(
+				"poll_id" => $poll_id,
+				"choice_text" => $poll_information['poll_choice2'],
+				"created_at" => date("Y-m-d H:i:s"),
+				"updated_at" => date("Y-m-d H:i:s")							
+			),
+			array(
+				"poll_id" => $poll_id,
+				"choice_text" => $poll_information['poll_choice3'],
+				"created_at" => date("Y-m-d H:i:s"),
+				"updated_at" => date("Y-m-d H:i:s")
+			),
+			array(
+				"poll_id" => $poll_id,
+				"choice_text" => $poll_information['poll_choice1'],
+				"created_at" => date("Y-m-d H:i:s"),
+				"updated_at" => date("Y-m-d H:i:s")
+			)
+		);
 
 		return $this->db->insert_batch('choices', $choice_data);
 	}
